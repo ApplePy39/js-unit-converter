d95ac625ebf3284f0fd8dfe2b1ae13783919e150
Anton Frank <antonfrank330@gmail.com>
2022-01-24 21:03:07 -0600

    Added a header (unfinished)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import DropdownMenu from './components/DropdownMenu';
 
 function App() {
   return (
     <div className="App">
-      <h1>Js Unit Converter</h1>
+      <Header />
+      <DropdownMenu />
     </div>
   );
 }
